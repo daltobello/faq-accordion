ae277fade68ce49d8b94eb03d56f5207ad87a9a9
Ann Hochworter <ann.hochworter@gmail.com>
2024-03-21 09:48:48 -0600

    Move the mapping of the array to App.js

--- src/components/AccordionCard/AccordionCard.jsx
+++ src/components/AccordionCard/AccordionCard.jsx
@@ -1,25 +1,19 @@
 import React from 'react';
+import { useState } from 'react';
 import './AccordionCard.css';
 
-function AccordionCard({accordionFacts}) {
-  const accordionInfo = accordionFacts.map((fact) => {
-    return (
-      <div className='accordion-facts'>
-      <section>
-        <h3 className='accordion-title'>{fact.title}</h3>
-      </section>
-      <article>
-        <p className='accordion-content'>{fact.content}</p>
-      </article>
-      </div>
-    )
-  })
+function AccordionCard({ title, content }) {
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div className='accordion-card-container'>
-      <h1>FAQs</h1>
-      <h2>What is Frontend Mentor, and how will it help me?</h2>
-      {accordionInfo}
+    <div className="accordion-facts">
+      <section onClick={() => setIsOpen(!isOpen)}>
+        <h3 className="accordion-title">{title}</h3>
+        <div>{isOpen ? '-' : '+'}</div>
+      </section>
+      <article>
+        {isOpen && <p className="accordion-content">{content}</p>}
+      </article>
     </div>
   );
 }
