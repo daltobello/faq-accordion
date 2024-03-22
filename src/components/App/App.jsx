69e4b676e793ed384e0bcdcf1723d762a265a2be
Ann Hochworter <ann.hochworter@gmail.com>
2024-03-22 11:31:18 -0600

    Adjust style to match comp

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,18 +2,21 @@ import { useState } from 'react';
 import accordionData from '../../AccordionData/AccordionData';
 import AccordionCard from '../AccordionCard/AccordionCard';
 import './App.css';
-import Header from '../Header/Header';
+// import Header from '../Header/Header';
 
 function App() {
   const [accordionFacts, setAccordionFacts] = useState(accordionData);
 
   return (
-    <div className='app'>
-      <Header />
-      <div className='accordion-wrapper'>
-        {accordionFacts.map(({ title, content }) => (
-          <AccordionCard title={title} content={content} />
-        ))}
+    <div className="app">
+      {/* <Header /> */}
+      <div className="accordion-wrapper">
+        <div className="accordion">
+          <h1 className="faqs-heading">FAQs</h1>
+          {accordionFacts.map(({ title, content }) => (
+            <AccordionCard title={title} content={content} />
+          ))}
+        </div>
       </div>
     </div>
   );
