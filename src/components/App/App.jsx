9010163b809cf7296280f20417fbddcbb332f750
Devin Altobello <130494366+alto-fx@users.noreply.github.com>
2024-03-25 16:56:56 -0400

    Remove header component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,18 +1,19 @@
-import { useState } from 'react';
-import accordionData from '../../AccordionData/AccordionData';
-import AccordionCard from '../AccordionCard/AccordionCard';
-import './App.css';
-// import Header from '../Header/Header';
+import { useState } from "react";
+import accordionData from "../../AccordionData/AccordionData";
+import AccordionCard from "../AccordionCard/AccordionCard";
+import "./App.css";
 
 function App() {
   const [accordionFacts, setAccordionFacts] = useState(accordionData);
 
   return (
-    <div className="app">
-      {/* <Header /> */}
-      <div className="accordion-wrapper">
-        <div className="accordion">
-          <h1 className="faqs-heading">FAQs</h1>
+    <div className='app'>
+      <div className='accordion-wrapper'>
+        <div className='accordion'>
+          <div className="title-wrapper">
+            <img src='src/assets/images/icon-star.svg' alt='star icon' />
+            <h1 className='faqs-heading'>FAQs</h1>
+          </div>
           {accordionFacts.map(({ title, content }) => (
             <AccordionCard title={title} content={content} />
           ))}
