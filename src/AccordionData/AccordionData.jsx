9aea756ff6126c70286d831cf2a9cf3f21b8bd56
Devin Altobello <130494366+alto-fx@users.noreply.github.com>
2024-03-30 10:40:26 -0400

    Add id property

--- src/AccordionData/AccordionData.jsx
+++ src/AccordionData/AccordionData.jsx
@@ -4,6 +4,7 @@ const accordionData = [
     content: `Paris, the City of Light, is famous for its art, fashion, gastronomy, and culture. 
     From iconic landmarks like the Eiffel Tower and Notre-Dame Cathedral to world-renowned museums 
     such as the Louvre and Musée d'Orsay, there's no shortage of things to see and do in Paris.`,
+    id: 1
   },
   {
     title: 'Kyoto, Japan',
@@ -11,6 +12,7 @@ const accordionData = [
     temples, as well as gardens, imperial palaces, Shinto shrines, and traditional wooden houses. 
     The city's historic charm combined with its beautiful scenery makes it a favorite destination 
     for travelers.`,
+    id: 2
   },
   {
     title: 'Santorini, Greece',
@@ -18,6 +20,7 @@ const accordionData = [
     roofs, and crystal-clear waters. Visitors can explore picturesque villages, relax on beautiful 
     beaches, and indulge in delicious Greek cuisine while enjoying breathtaking views of the 
     Aegean Sea.`,
+    id: 3
   },
   {
     title: 'New York City, USA',
@@ -25,6 +28,7 @@ const accordionData = [
     entertainment, and diversity. From iconic landmarks like Times Square and Central Park to 
     world-class museums, Broadway shows, and a vibrant culinary scene, there's always something 
     exciting to discover in NYC.`,
+    id: 4
   },
   {
     title: 'Machu Picchu, Peru',
@@ -32,6 +36,7 @@ const accordionData = [
     one of the most iconic archaeological sites in the world. Visitors can hike the Inca Trail, 
     explore the ruins, and marvel at the breathtaking panoramic views of the surrounding 
     mountains and lush cloud forest.`,
+    id: 5
   },
 ];
 
