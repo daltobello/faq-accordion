46cd9746f2eda7f40b97d6147ff26100934423f9
Devin Altobello <130494366+alto-fx@users.noreply.github.com>
2024-03-30 12:27:32 -0400

    Refactor list and item into separate components

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -8,19 +8,28 @@ function App() {
 
   return (
     <div className='app'>
-      <div className='accordion-wrapper'>
-        <div className='accordion'>
-          <div className="title-wrapper">
-            <img src='src/assets/images/icon-star.svg' alt='star icon' />
-            <h1 className='faqs-heading'>FAQs</h1>
-          </div>
-          {accordionFacts.map(({ title, content, id }) => (
-            <AccordionCard key={id} title={title} content={content} />
-          ))}
-        </div>
-      </div>
+      <AccordionList items={accordionFacts} />
     </div>
   );
 }
+
+const AccordionList = ({ items }) => (
+  <div className='accordion-wrapper'>
+    <div className='accordion'>
+      <TitleWrapper />
+      {items.map(({ id, title, content}) => (
+        <AccordionCard key={id} title={title} content={content} />
+      ))}
+    </div>
+  </div>
+);
+
+const TitleWrapper = () => (
+  <div className='title-wrapper'>
+    <img src='src/assets/images/icon-star.svg' alt='star icon' />
+    <h1 className='faqs-heading'>FAQs</h1>
+  </div>
+);
+
 
 export default App;
