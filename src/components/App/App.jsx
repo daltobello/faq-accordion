df66405b9e49f92300282c9c59ececa5b3f1e08c
Devin Altobello <130494366+alto-fx@users.noreply.github.com>
2024-04-01 20:10:13 -0400

    Add scroll-area div

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,33 +1,35 @@
-import { useState } from 'react';
-import AccordionCard from '../AccordionCard/AccordionCard';
-import accordionData from '../../AccordionData/AccordionData';
-import './App.css';
+import { useState } from "react";
+import AccordionCard from "../AccordionCard/AccordionCard";
+import accordionData from "../../AccordionData/AccordionData";
+import "./App.css";
 
 function App() {
   const [accordionFacts] = useState(accordionData);
 
   return (
-    <div className="app">
+    <div className='app'>
       <AccordionList items={accordionFacts} />
     </div>
   );
 }
 
 const AccordionList = ({ items }) => (
-  <div className="accordion-wrapper">
-    <div className="accordion">
+  <div className='accordion-wrapper'>
+    <div className='accordion'>
       <TitleWrapper />
-      {items.map(({ id, title, content }) => (
-        <AccordionCard key={id} title={title} content={content} />
-      ))}
+      <div className='scroll-area'>
+        {items.map(({ id, title, content }) => (
+          <AccordionCard key={id} title={title} content={content} />
+        ))}
+      </div>
     </div>
   </div>
 );
 
 const TitleWrapper = () => (
-  <div className="title-wrapper">
-    <img src="src/assets/images/icon-star.svg" alt="star icon" />
-    <h1 className="faqs-heading">FAQs</h1>
+  <div className='title-wrapper'>
+    <img src='src/assets/images/icon-star.svg' alt='star icon' />
+    <h1 className='faqs-heading'>FAQs</h1>
   </div>
 );
 
